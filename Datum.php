6bf30b2f6cf074c879230abc3dc6beb84b4e3b71
max19082005 <129419212+max19082005@users.noreply.github.com>
2023-03-30 21:37:44 +0200

    Add files via upload

---
+++ Datum.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>hallo allemaal, dit is een site gemaakt door Maxim Janssen en deze site toont de datum van vandaag in php</title>
+</head>
+<body>
+  <?php
+    // Print "Hello World" three times, each on a new line
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+
+    // Print the current date in the format "Day Month Year"
+    echo date("l j F Y");
+  ?>
+</body>
+</html>
